e9a04e51be28afe58bee0d3d5665fa211053e594
yql <1014796661@qq.com>
2023-06-02 14:32:46 +0800

    测试1
    (cherry picked from commit b5ee8d4cc7b2ae10d2ca435da074693efefefb54)

--- src/main/java/com/example/gittest/test.java
+++ src/main/java/com/example/gittest/test.java
@@ -2,6 +2,7 @@ package com.example.gittest;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println(""asd);
+        System.out.println("asd");
+        System.out.println("asd1");
     }
 }
